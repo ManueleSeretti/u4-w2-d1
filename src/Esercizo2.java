013182b0c48cc22ee991a1db72df429e8bb92012
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-10-09 15:54:04 +0200

    esercizio2

--- src/Esercizo2.java
+++ src/Esercizo2.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Esercizo2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("inserisci i km percorsi:");
+                int km = Integer.parseInt(input.nextLine());
+                if (km == 0) throw new ArithmeticException("hai inserito 0");
+                System.out.println("inserisci i litri di carburante consumati:");
+                int lt = Integer.parseInt(input.nextLine());
+                if (lt == 0) throw new ArithmeticException("hai inserito 0");
+                int consumo = km / lt;
+                System.out.println("il consumo è di :" + consumo + "  km/lt");
+                break;
+            } catch (Exception x) {
+                System.err.println(x.getMessage());
+            }
+        }
+        input.close();
+    }
 }
